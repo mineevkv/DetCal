2fadadf14ff6f57932106c383918582b81fcf2dc
DESKTOP-LKQKK60\KM <mineevkv@gmail.com>
2025-10-08 22:15:19 +0300

    Sheets was refactored

--- GUI/Sheets/abstract_sheet.py
+++ GUI/Sheets/abstract_sheet.py
@@ -21,7 +21,28 @@ logger = get_logger(__name__)
 
 
 class Sheet(ABC):
-    def __init__(self, main_window):
+    """
+    Abstract base class for all sheets.
+
+    Provides common functionality for all sheets.
+
+    Attributes:
+        layout (QLayout): The layout of the main window.
+        _margin_top (int): The top margin of the sheet.
+        _margin_left (int): The left margin of the sheet.
+        _zero_col (int): The column where the sheet starts.
+        _zero_row (int): The row where the sheet starts.
+        _row_hight (int): The height of a row in the sheet.
+        _col_width (int): The width of a column in the sheet.
+        elem (dict): A dictionary of elements in the sheet.
+        _elem_hight (int): The height of an element in the sheet.
+        box (QGroupBox): The group box of the sheet.
+        x_col (list): A list of x coordinates of the columns in the sheet.
+        y_row (list): A list of y coordinates of the rows in the sheet.
+
+    """
+
+    def __init__(self, main_window) -> None:
         self.layout = main_window.get_layout()
         self._margin_top = 10
         self._margin_left = 11
@@ -29,27 +50,19 @@ class Sheet(ABC):
         self._zero_col = 0
         self._zero_row = 0
 
-        self._row_hight = 25
+        self._row_height = 25
         self._col_width = 10
 
         self.elem = getattr(self, "elem", {})  # existing condition
-        self._elem_hight = 20
+        self._elem_height = 20
 
         self.box = QGroupBox("Empty sheet")  # existing condition
 
         # Default grid for placing elements
-        self.x_col = [
-            x
-            for x in range(
-                self.margin_left, main_window.width() - self.margin_left, self.col_width
-            )
-        ]
-        self.y_row = [
-            y
-            for y in range(
-                self.margin_top, main_window.height() - self.margin_top, self.row_hight
-            )
-        ]
+        right_end = main_window.width() - self.margin_left
+        bottom_end = main_window.height() - self.margin_top
+        self.x_col = [x for x in range(self.margin_left, right_end, self.col_width)]
+        self.y_row = [y for y in range(self.margin_top, bottom_end, self.row_height)]
 
     @property
     def col_width(self) -> int:
@@ -57,9 +70,9 @@ class Sheet(ABC):
         return self._col_width
 
     @property
-    def row_hight(self) -> int:
+    def row_height(self) -> int:
         """Return the height of a row in the sheet"""
-        return self._row_hight
+        return self._row_height
 
     @property
     def margin_top(self) -> int:
@@ -82,86 +95,128 @@ class Sheet(ABC):
         return self._zero_row
 
     @property
-    def elem_hight(self) -> int:
+    def elem_height(self) -> int:
         """Return the height of an element in the sheet"""
-        return self._elem_hight
+        return self._elem_height
 
-    def get_widget(self):
+    def get_widget(self) -> QWidget:
         """Return the main widget of the sheet"""
         return self.box
 
-    def hide(self):
+    def hide(self) -> None:
+        """Hide the sheet"""
         self.box.hide()
 
-    def add_label(self, key, col, row, text, width=85):
+    def add_label(
+        self, key: str, col: int, row: int, text: str, width: int = 85
+    ) -> QLabel:
+        """Add a QLabel to the sheet"""
         label = self.elem[f"{key}_LABEL"] = QLabel(f"{text}", parent=self.box)
         label.setGeometry(
-            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_hight)
+            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_height)
         )
         return label
 
-    def add_line_edit(self, key, col, row, value, width=63):
+    def add_line_edit(
+        self, key: str, col: int, row: int, value: float, width: int = 63
+    ) -> QLineEdit:
+        """Add a QLineEdit to the sheet"""
         line_edit = self.elem[f"{key}_LINE"] = QLineEdit(f"{value}", parent=self.box)
         line_edit.setGeometry(
-            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_hight)
+            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_height)
         )
         line_edit.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
         return line_edit
 
-    def add_clickable_line_edit(self, key, col, row, value, width=63):
+    def add_clickable_line_edit(
+        self, key: str, col: int, row: int, value: float, width: int = 63
+    ) -> ClickableLineEdit:
+        """Add a ClickableLineEdit (QLineEdit with a click event)  to the sheet"""
         line_edit = self.elem[f"{key}_CLICKLINE"] = ClickableLineEdit(
             f"{value}", parent=self.box
         )
         line_edit.setGeometry(
-            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_hight)
+            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_height)
         )
         line_edit.setProperty("class", "ip_line")
         line_edit.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
         return line_edit
 
-    def add_btn(self, key, col, row, text, width=60):
+    def add_btn(
+        self, key: str, col: int, row: int, text: str, width: int = 60
+    ) -> QPushButton:
+        """Add a QPushButton to the sheet"""
         btn = self.elem[f"BTN_{key}"] = QPushButton(f"{text}", parent=self.box)
         btn.setGeometry(
-            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_hight)
+            QtCore.QRect(self.x_col[col], self.y_row[row], width, self.elem_height)
         )
         return btn
 
-    def add_custom_btn(self, key, col, row, text, width, hight, elem_class=None):
+    def add_custom_btn(
+        self,
+        key: str,
+        col: int,
+        row: int,
+        text: str,
+        width: int,
+        height: int,
+        elem_class: str = None,
+    ) -> QPushButton:
+        """Add a custom QPushButton with a specific class to the sheet"""
         btn = self.add_btn(key, col, row, text, width)
-        btn.setFixedHeight(hight)
+        btn.setFixedHeight(height)
         if elem_class is not None:
             btn.setProperty("class", elem_class)
         return btn
 
-    def add_check_box(self, key, col, row, text):
+    def add_check_box(self, key: str, col: int, row: int, text: str) -> QCheckBox:
+        """Add a QCheckBox to the sheet"""
         check_box = self.elem[key] = QCheckBox(parent=self.box, text=text)
         check_box.setGeometry(QtCore.QRect(self.x_col[col], self.y_row[row], 200, 20))
         return check_box
 
-    def add_radio_btn(self, key, col, row, text, width=90):
+    def add_radio_btn(
+        self, key: str, col: int, row: int, text: str, width: int = 90
+    ) -> QRadioButton:
+        """Add a QRadioButton to the sheet"""
         radio_btn = self.elem[key] = QRadioButton(parent=self.box, text=text)
         radio_btn.setGeometry(QtCore.QRect(self.x_col[col], self.y_row[row], width, 20))
         radio_btn.setChecked(False)
         return radio_btn
 
-    def add_progress_bar(self, key, col, row, width, hight):
+    def add_progress_bar(
+        self, key: str, col: int, row: int, width: int, height: int
+    ) -> QProgressBar:
+        """Add a QProgressBar to the sheet"""
         progress_bar = self.elem[key] = QProgressBar(parent=self.box)
         progress_bar.setGeometry(
-            QtCore.QRect(self.x_col[col], self.y_row[row], width, hight)
+            QtCore.QRect(self.x_col[col], self.y_row[row], width, height)
         )
         progress_bar.setRange(0, 100)
         return progress_bar
 
-    def shift_position(self, elem, shift_x=0, shift_y=0):
+    def shift_position(self, elem: object, shift_x: int = 0, shift_y: int = 0) -> None:
+        """Shift the (dx, dy)position of an element"""
         elem.move(elem.x() + shift_x, elem.y() + shift_y)
 
-    def add_frame(self, key, col, row, dx, dy, width, hight, classname):
+    def add_frame(
+        self,
+        key: str,
+        col: int,
+        row: int,
+        dx: int,
+        dy: int,
+        width: int,
+        height: int,
+        class_name: str,
+    ) -> None:
+        """Add a frame to the sheet"""
         frame = self.elem[f"{key}_FRAME"] = self.frame_ch = QWidget(self.box)
-        frame.setProperty("class", f"{classname}")
-        frame.setGeometry(self.x_col[col] - dx, self.y_row[row] - dy, width, hight)
+        frame.setProperty("class", f"{class_name}")
+        frame.setGeometry(self.x_col[col] - dx, self.y_row[row] - dy, width, height)
         frame.setAutoFillBackground(True)
 
-    def cleanup(self):
+    def cleanup(self) -> None:
         """Safe cleanup method to be called explicitly"""
         if hasattr(self, "box") and self.box:
             self.box.deleteLater()
--- GUI/Sheets/infographic_sheet.py
+++ GUI/Sheets/infographic_sheet.py
@@ -1,53 +1,66 @@
 from .abstract_sheet import Sheet
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import (
-    QWidget, QPushButton, QApplication, 
-    QGridLayout, QLabel, QLineEdit, QGroupBox)
-from PyQt6.QtGui import QPen
-from PyQt6.QtCore import QMutex
-
-from GUI.palette import *
+from PyQt6 import QtCore
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.animation import FuncAnimation
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from PyQt6.QtWidgets import  QGroupBox
+from PyQt6.QtWidgets import QVBoxLayout, QWidget, QComboBox
 
 from GUI.palette import *
-from GUI.QtCustomWidgets.custom_widgets import *
 
 from System.logger import get_logger
+
 logger = get_logger(__name__)
 
-class InfographicSheet(Sheet):
 
-    def __init__(self, main_layout):
+class InfographicSheet(Sheet):
+    """
+    Class for the infographic sheet in the main window.
+
+    This class is used to create the infographic sheet in the main window.
+    It contains a plot for the power levels vs output voltages from detector.
+    """
+
+    def __init__(self, main_layout) -> None:
         super().__init__(main_layout)
-        
-        self.box.setTitle("Infografic")
+
+        self.box.setTitle("Infographic")
 
         self.add_plot_sheet()
 
         width_combo = 120
         row_combo = 2
-        row_detname =row_combo + 2
+        row_detname = row_combo + 2
         row_btn = row_detname + 2
 
-        self.add_frequency_selector('FREQ_COBMO',self.zero_col, row_combo, width_combo, self.elem_hight)
-        self.add_line_edit('DET_NAME', self.zero_col, row_detname, 'rf_det_name', width_combo).setProperty('class', 'det_name_line')
-        self.add_custom_btn('PROTOCOL', self.zero_col, row_btn , 'Generate Protocol', width_combo, 45, 'btn_protocol')
+        self.add_frequency_selector(
+            "FREQ_COMBO", self.zero_col, row_combo, width_combo, self.elem_height
+        )
+        self.add_line_edit(
+            "DET_NAME", self.zero_col, row_detname, "rf_det_name", width_combo
+        ).setProperty("class", "det_name_line")
+        self.add_custom_btn(
+            "PROTOCOL",
+            self.zero_col,
+            row_btn,
+            "Generate Protocol",
+            width_combo,
+            45,
+            "btn_protocol",
+        )
 
-
-    def add_frequency_selector(self, key, col, row, width, hight):
-        self.elem[key] = QtWidgets.QComboBox(parent=self.box)
-        self.elem[key].setGeometry(QtCore.QRect(self.x_col[col], self.y_row[row], width, hight))
+    def add_frequency_selector(
+        self, key: str, col: int, row: int, width: int, height: int
+    ) -> QComboBox:
+        """Add a combo box for frequency selection."""
+        self.elem[key] = QComboBox(parent=self.box)
+        self.elem[key].setGeometry(
+            QtCore.QRect(self.x_col[col], self.y_row[row], width, height)
+        )
         return self.elem[key]
 
-
-    def add_plot_sheet(self):  
+    def add_plot_sheet(self) -> None:
+        """Add a plots for the power levels vs output voltages from detector."""
         # Create a proper container for the plot
         plot_container1 = QWidget(parent=self.box)
         plot_container1.setStyleSheet("background-color: none; border: none;")
@@ -55,7 +68,9 @@ class InfographicSheet(Sheet):
         plot_container1.setLayout(plot_layout1)
         # Set geometry to ensure visibility
         dx = 120
-        plot_container1.setGeometry(QtCore.QRect(self.x_col[self.zero_col] + dx, 20, 380, 200))
+        plot_container1.setGeometry(
+            QtCore.QRect(self.x_col[self.zero_col] + dx, 20, 380, 200)
+        )
         self.figure1 = PlotFigure(plot_layout1)
 
         # Create a proper container for the plot
@@ -64,27 +79,29 @@ class InfographicSheet(Sheet):
         plot_layout2 = QVBoxLayout(plot_container2)
         plot_container2.setLayout(plot_layout2)
         # Set geometry to ensure visibility
-        plot_container2.setGeometry(QtCore.QRect(self.x_col[self.zero_col] + 350 + dx, 20, 380, 200))
+        plot_container2.setGeometry(
+            QtCore.QRect(self.x_col[self.zero_col] + 350 + dx, 20, 380, 200)
+        )
         self.figure2 = PlotFigure(plot_layout2)
-        self.figure2.ax.set_xlabel('SA input, dBm', fontsize=8)
+        self.figure2.ax.set_xlabel("SA input, dBm", fontsize=8)
 
+matplotlib.use('Qt5Agg') # Qt5Agg for PyQt6
 
-# Qt5Agg for PyQt6
-matplotlib.use('Qt5Agg')  # TODO: check without this
+class PlotFigure:
+    """Class for the plot figure."""
 
-class PlotFigure():
-    def __init__(self, layout):
+    def __init__(self, layout) -> None:
         self.layout = layout
         self.meas_data = []
-   
+
         self.figure, self.ax = plt.subplots()
         self.canvas = FigureCanvas(self.figure)
         self.canvas.setStyleSheet("background-color: transparent; border: none;")
         self.layout.addWidget(self.canvas)
-        
+
         self.figure.subplots_adjust(left=0.2, right=0.95, bottom=0.25, top=0.95)
-        self.ax.set_xlabel('Gen output, dBm', fontsize=8)
-        self.ax.set_ylabel('Voltage, mV', fontsize=8)
+        self.ax.set_xlabel("Gen output, dBm", fontsize=8)
+        self.ax.set_ylabel("Voltage, mV", fontsize=8)
         self.figure.patch.set_alpha(0)
         self.ax.patch.set_alpha(0)
 
@@ -93,49 +110,61 @@ class PlotFigure():
         self.ax.xaxis.label.set_color(YELLOW)
         self.ax.yaxis.label.set_color(YELLOW)
 
-        self.ax.tick_params(axis='both', which='major', labelsize=8)
-        self.ax.tick_params(axis='x', colors=YELLOW)
-        self.ax.tick_params(axis='y', colors=YELLOW)
+        self.ax.tick_params(axis="both", which="major", labelsize=8)
+        self.ax.tick_params(axis="x", colors=YELLOW)
+        self.ax.tick_params(axis="y", colors=YELLOW)
 
         # self.ax.set_xlim(-20, 15)
         self.ax.set_ylim(0, 500)
 
-        self.ax.spines['bottom'].set_color(BLUE)
-        self.ax.spines['left'].set_color(BLUE)
-        self.ax.spines['top'].set_color(BLUE)
-        self.ax.spines['right'].set_color(BLUE)
-        
+        self.ax.spines["bottom"].set_color(BLUE)
+        self.ax.spines["left"].set_color(BLUE)
+        self.ax.spines["top"].set_color(BLUE)
+        self.ax.spines["right"].set_color(BLUE)
 
-        self.max_points = 300  
+        self.max_points = 300
         self.x, self.y = [], []
-  
-        # Initialize with empty data
-        self.line, = self.ax.plot([], [], '-', color=YELLOW, linewidth=2)
-        
 
-    def add_point(self, x, y, autoscale=False):
+        # Initialize with empty data
+        (self.line,) = self.ax.plot([], [], "-", color=YELLOW, linewidth=2)
+
+    def add_point(self, x: float, y: float, autoscale=False) -> None:
+        """
+        Add a point to the plot.
+
+        Parameters
+        ----------
+        x : float
+            x-coordinate of the point to add
+        y : float
+            y-coordinate of the point to add
+        autoscale : bool, optional
+            Whether to autoscale the plot after adding the point, by default False
+        """
         self.x = np.append(self.x, x)
         self.y = np.append(self.y, y)
         self.line.set_data(self.x, self.y)
         # self.ax.set_xlim(-20, 15)
-        self.ax.set_ylim(0, max(self.y)*1.2)
-        if autoscale:
-            self.ax.relim()
-            self.ax.autoscale_view()
-        self.canvas.draw_idle()
-    
-    def plot_line(self, x, y, autoscale=False):
-        self.x = np.asarray(x)
-        self.y = np.asarray(y)
-        self.line.set_data(self.x, self.y)
-        # self.ax.set_xlim(-20, 15)
-        self.ax.set_ylim(0, max(self.y)*1.2)
+        self.ax.set_ylim(0, max(self.y) * 1.2)
         if autoscale:
             self.ax.relim()
             self.ax.autoscale_view()
         self.canvas.draw_idle()
 
-    def clear_plot(self):
+    def plot_line(self, x, y, autoscale=False) -> None:
+        """Plot a line on the plot."""
+        self.x = np.asarray(x)
+        self.y = np.asarray(y)
+        self.line.set_data(self.x, self.y)
+        # self.ax.set_xlim(-20, 15)
+        self.ax.set_ylim(0, max(self.y) * 1.2)
+        if autoscale:
+            self.ax.relim()
+            self.ax.autoscale_view()
+        self.canvas.draw_idle()
+
+    def clear_plot(self) -> None:
+        """Clear the plot."""
         self.x = []
         self.y = []
         self.line.set_data(self.x, self.y)
